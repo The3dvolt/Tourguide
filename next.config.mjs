0182368cb2530652694d23979d69c2bc2d7fd36f
Mario Perez <3dvolt@gmail.com>
2026-01-13 20:53:08 -0500

    FIX: Explicitly install voice dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    // This setting is required for @xenova/transformers to work on Vercel
     serverExternalPackages: ['@xenova/transformers'],
 };
 
